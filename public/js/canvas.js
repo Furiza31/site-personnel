4479b3b3b23b94b48520bf1a4db3ef8a742e3a8c
Furiza31 <hugo.wendjaneh@gmail.com>
2022-03-27 18:10:06 +0200

    wave animation

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,5 +1,10 @@
 let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext('2d');
+let degree = 0;
+const amplitude = 30;
+const period = 1000;
+const speed = 2;
+
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
@@ -7,3 +12,38 @@ window.addEventListener("resize", () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 });
+
+if (canvas.getContext) {
+    let ctx = canvas.getContext('2d');
+    ctx.strokeStyle = 'black';
+
+    function drawWave() {
+        // calculates the slope of the curve
+        let pante = canvas.height / canvas.width;
+
+        // clear canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        ctx.beginPath();
+        // make a triangle
+        ctx.moveTo(canvas.width, 0);
+        ctx.lineTo(0, 0);
+        ctx.lineTo(0, canvas.height);
+        for (let x = 0, panteAdd = 0; x <= canvas.width; x++, panteAdd += pante) {
+            // calc the courbe of sinus for the wave
+            let y = -amplitude * Math.sin((Math.PI / period) * (degree + x));
+            ctx.lineTo(x, y + canvas.height - panteAdd);
+        }
+        ctx.fill();
+        ctx.closePath();
+        // reset memory
+        if (degree == 2000) {
+            degree = 0;
+        }
+        degree += speed;
+        window.requestAnimationFrame(drawWave);
+    }
+
+    drawWave();
+
+}
