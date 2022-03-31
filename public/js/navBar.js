27d71640e4076487231c4b6c6b2dd5efbc066d83
Furiza31 <hugo.wendjaneh@gmail.com>
2022-03-31 19:28:31 +0200

    ajout de l'évenement selected sur la navBar + debut de creation des skills

---
+++ public/js/navBar.js
@@ -0,0 +1,29 @@
+let header = document.getElementById("header");
+let headerLinks = header.querySelectorAll("a");
+
+for (let i = 0; i < headerLinks.length - 1; i++) {
+    if (i == 0) {
+        headerLinks[i].addEventListener("click", function() {
+            for (let j = 0; j < headerLinks.length; j++) {
+                headerLinks[j].classList.remove("active");
+            }
+            this.classList.add("active");
+            headerLinks[i + 1].classList.add("active");
+        });
+    } else if (i == 1) {
+        headerLinks[i].addEventListener("click", function() {
+            for (let j = 0; j < headerLinks.length; j++) {
+                headerLinks[j].classList.remove("active");
+            }
+            this.classList.add("active");
+            headerLinks[i - 1].classList.add("active");
+        });
+    } else {
+        headerLinks[i].addEventListener("click", function() {
+            for (let j = 0; j < headerLinks.length; j++) {
+                headerLinks[j].classList.remove("active");
+            }
+            this.classList.add("active");
+        });
+    }
+}
