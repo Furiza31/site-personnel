640257fda142deb62d334448b42ef4801ead1f5a
Furiza31 <hugo.wendjaneh@gmail.com>
2022-04-05 20:41:49 +0200

    class active sur Accueil + faute orthographe

--- public/js/navBar.js
+++ public/js/navBar.js
@@ -3,6 +3,8 @@ let headerLinks = header.querySelectorAll("a");
 
 for (let i = 0; i < headerLinks.length - 1; i++) {
     if (i == 0) {
+        headerLinks[i + 1].classList.add("active");
+        headerLinks[i].classList.add("active");
         headerLinks[i].addEventListener("click", function() {
             for (let j = 0; j < headerLinks.length; j++) {
                 headerLinks[j].classList.remove("active");
